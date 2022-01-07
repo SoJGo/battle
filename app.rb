5fe7f763b00ec04ff5b60006c0f2a15d531feefe
Jason Humbert <jasonhumbert@outlook.com>
2022-01-07 16:49:22 +0000

    add redirect and /play route

--- app.rb
+++ app.rb
@@ -6,14 +6,23 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player1, @player2 = params[:player1], params[:player2]
+    session[:player1], session[:player2] = params[:player1], params[:player2]
+    
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player1, @player2 = session[:player1], session[:player2]
+    
     erb(:play)
-  end    
+  end
 
   run! if app_file == $0
 end
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -5,8 +5,6 @@ feature 'Enter names' do
       fill_in('player2', with: 'Sophie')
       click_button('Commence')
 
-      save_and_open_page
-
       expect(page).to have_content 'Jason vs. Sophie'
     end
   end
