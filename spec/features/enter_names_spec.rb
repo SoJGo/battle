96537f3d0671a703a2c71b7d086c6255a15d7d75
SophieGowen <sophie.j.gowen@gmail.com>
2022-01-07 16:29:09 +0000

    Add title to index view and remanme tests

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,12 @@
-feature 'Testing submitting names' do
-    scenario 'can see names loaded' do
+feature 'Enter names' do
+    scenario 'submitting names' do
       visit('/')
       fill_in('player1', with: 'Jason')
       fill_in('player2', with: 'Sophie')
       click_button('Commence')
+
+      save_and_open_page
+
       expect(page).to have_content 'Jason vs. Sophie'
     end
   end
