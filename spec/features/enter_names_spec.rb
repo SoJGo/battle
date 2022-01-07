faf121b458718b4991a9289417185346bd4dbecf
Jason Humbert <jasonhumbert@outlook.com>
2022-01-07 17:52:31 +0000

    refactor using web helper

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
     scenario 'submitting names' do
-      visit('/')
-      fill_in('player1', with: 'Jason')
-      fill_in('player2', with: 'Sophie')
-      click_button('Commence')
+      sign_in_and_play
       expect(page).to have_content 'Jason vs. Sophie'
     end
   end
--- spec/features/view_hit_points_spec.rb
+++ spec/features/view_hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'View hit points' do
   scenario 'see Player 2 hit points' do
-    visit('/')
-      fill_in('player1', with: 'Jason')
-      fill_in('player2', with: 'Sophie')
-      click_button('Commence')
-      expect(page).to have_content 'Sophie HP: 100/100'
+    sign_in_and_play
+    expect(page).to have_content 'Sophie HP: 100/100'
   end
 end
